ff7517b15643529a7f79a3e33363ab9b9c02c991
DavidRenda12 <david_renda@yahoo.com>
2026-03-06 11:32:41 -0800

    Auto-update jobs

--- run_scraper.bat
+++ run_scraper.bat
@@ -1,8 +1,6 @@
 @echo off
 cd /d "C:\Users\David\Desktop\MyWebsiteLocal\Test"
 python scraper_greenhouse.py
-python scraper_sacramento.py
-python combine_jobs.py
 git add .
 git commit -m "Auto-update jobs"
 git push
