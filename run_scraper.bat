f3516e5790f08a1f52604fc62d0dd898c828f3f0
DavidRenda12 <david_renda@yahoo.com>
2026-03-06 12:11:34 -0800

    Add scrape timestamp tracking

--- run_scraper.bat
+++ run_scraper.bat
@@ -4,4 +4,8 @@ python scraper_greenhouse.py
 git add .
 git commit -m "Auto-update jobs"
 git push
+echo %date% %time% > scrape_time.txt
+git add scrape_time.txt
+git commit -m "Update scrape timestamp"
+git push
 pause
